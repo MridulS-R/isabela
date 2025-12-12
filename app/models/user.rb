30ec1fb58b804d44361fd4269f6d558e381c0bd9
MridulS-R <sr.mridulshukla9@gmail.com>
2025-12-13 03:34:30 +0530

    update

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :posts, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
   has_secure_password
   has_one_attached :avatar
 
