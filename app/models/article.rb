6188d5826256b2e6baabdd2b9d1d510fc19ba9f9
MridulS-R <sr.mridulshukla9@gmail.com>
2025-12-13 17:50:10 +0530

    updating images

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,5 +3,10 @@ class Article < ApplicationRecord
   validates :body, presence: true
 
   scope :published, -> { where.not(published_at: nil).order(published_at: :desc) }
-end
 
+  def first_image_url
+    return nil if body.blank?
+    m = body.to_s.match(/<img[^>]+src=["']([^"']+)["']/i)
+    m && m[1]
+  end
+end
